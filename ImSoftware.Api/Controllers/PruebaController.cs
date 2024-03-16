41d5dadd024929f850fcdfed98a1bfd9e0f09390
EduardoPoox <eduardopoox.99@gmail.com>
2024-03-16 12:48:26 -0600

    eepooxm- segundo endpoint

---
+++ ImSoftware.Api/Controllers/PruebaController.cs
@@ -0,0 +1,25 @@
+﻿using ImSoftware.Api.Controllers.Modelos;
+using ImSoftware.Api.Datos;
+using ImSoftware.Api.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImSoftware.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PruebaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<PruebaDTO> GetPruebas()
+        {
+            return PruebaStore.PruebaList;
+        }
+
+        [HttpGet("id:int")]
+        public PruebaDTO GetPrueba(int Id)
+        {
+            return PruebaStore.PruebaList.FirstOrDefault(p => p.Id == Id);
+        }
+    }
+}
